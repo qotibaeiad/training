3feca97f636fa7c4843e7576e22b2b24751afa8e
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-24 23:25:06 +0200

    trtiple number euqals zero

---
+++ question_inteview/find_triple_number_equalzero.py
@@ -0,0 +1,11 @@
+class Solution:
+    def __init__(self):
+        self.triplets = []
+
+    def find_triplets_with_zero_sum(self, nums):
+
+# Example usage:
+solution = Solution()
+nums = [-1, 0, 1, 2, -1, -4, -3, 3, 1, 1]
+result = solution.find_triplets_with_zero_sum(nums)
+print(result)
