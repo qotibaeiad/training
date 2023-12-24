be5d135c5a140d3218bd29ab9887a3db45bd2e53
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-25 01:56:54 +0200

    trtiple number euqals zero

--- question_inteview/Num_Of_Island.py
+++ question_inteview/Num_Of_Island.py
@@ -10,12 +10,12 @@ class Island:
             for j in range(columns):
                 if self.matrix[i][j] == '1':
                     cnt += 1
-                self.dfs(i, j)
+                    self.dfs(i, j)
         return cnt
     
     def dfs(self,i,j):
 	    rows=len(self.matrix[0])
-	    columns  =len(self.matrix)
+	    columns=len(self.matrix)
 	    if(0<=i<columns and 0<=j<rows and self.matrix[i][j]=='1'):
 		    self.matrix[i][j]='0'
 		    self.dfs(i+1,j)
@@ -27,8 +27,8 @@ class Island:
 Matrix = [
     ['1', '1', '1', '0', '0'],
     ['1', '1', '0', '1', '0'],
-    ['1', '1', '0', '0', '0'],
-    ['0', '0', '1', '0', '1']
+    ['1', '1', '0', '1', '0'],
+    ['0', '0', '1', '1', '1']
 ]
 M = Island(Matrix)
 print(M.Num_Of_Island())
--- question_inteview/find_triple_number_equalzero.py
+++ question_inteview/find_triple_number_equalzero.py
@@ -3,7 +3,7 @@ class Solution:
         self.triplets = []
 
     def find_triplets_with_zero_sum(self, nums):
-
+        
 # Example usage:
 solution = Solution()
 nums = [-1, 0, 1, 2, -1, -4, -3, 3, 1, 1]
