b8e7beb08afe3c8a40b876018dfc2e3174e5c36d
qotibamhamed <qotibaeiad98@gmail.com>
2023-12-26 15:01:26 +0200

    add prefix common problem

---
+++ question_inteview/Common_Prefix.py
@@ -0,0 +1,17 @@
+def Common_Prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+
+    for st in strs[1:]:
+        i = 0
+        while i < len(prefix) and i < len(st) and prefix[i] == st[i]:
+            i += 1
+        if i < len(prefix):
+            prefix = prefix[:i]
+        if not prefix:
+            return ""
+
+    return prefix
+example3 = Common_Prefix(["eiv","eiade", "eiad"])
+print(example3)
---
+++ question_inteview/s.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def numDecodings(self, s):
+        mapping = {}
+        current_char = 'A'
+        current_code_point = ord('A')
+        for i in range(26):
+            mapping[current_code_point - 64] = current_char
+            current_code_point += 1
+            current_char = chr(current_code_point)
+
+        for item in mapping.items():
+            print(item)
+
+solution = Solution()
+solution.numDecodings("123")
